eec90f5a338131f907b066d043f9f42d55ccf2ea
Emma Price <emmalinprice@gmail.com>
2018-02-10 10:53:23 -0800

    Add files via upload

---
+++ kromma.ino
@@ -0,0 +1,56 @@
+// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
+// released under the GPLv3 license to match the rest of the AdaFruit NeoPixel library
+
+#include <Adafruit_NeoPixel.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303.h>
+
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define PIN 13
+
+#define NUMPIXELS 3
+
+// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
+// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
+// example for more information on possible values.
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+int delayval = 500; // delay for half a second
+
+Adafruit_LSM303 lsm;
+
+void setup() {
+  //pixels.begin(); // This initializes the NeoPixel library.
+}
+
+void loop() {
+
+  /*for(int i=0;i<NUMPIXELS;i++){
+
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(255,255,255)); // Moderately bright green color.
+
+    pixels.show(); // This sends the updated pixel color to the hardware.
+
+    delay(delayval); // Delay for a period of time (in milliseconds).
+
+  }*/
+
+  lsm.read();
+  Serial.print("Accel X: "); Serial.print((int)lsm.accelData.x); Serial.print(" ");
+  Serial.print("Y: "); Serial.print((int)lsm.accelData.y);       Serial.print(" ");
+  Serial.print("Z: "); Serial.println((int)lsm.accelData.z);     Serial.print(" ");
+  Serial.print("Mag X: "); Serial.print((int)lsm.magData.x);     Serial.print(" ");
+  Serial.print("Y: "); Serial.print((int)lsm.magData.y);         Serial.print(" ");
+  Serial.print("Z: "); Serial.println((int)lsm.magData.z);       Serial.print(" ");
+  delay(1000);
+
+  /*pixels.setPixelColor(0, pixels.Color(0,0,255));
+  pixels.setPixelColor(1, pixels.Color(0,255,0));
+  pixels.setPixelColor(2, pixels.Color(255,0,0));
+  pixels.show();*/
+}
