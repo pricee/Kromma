d17c70733d5872b85cf2c4e62c834a437f2434e1
Emma Price <emmalinprice@gmail.com>
2018-03-08 11:01:57 -0800

    Added accelerometer

--- kromma.ino
+++ kromma.ino
@@ -3,54 +3,53 @@
 
 #include <Adafruit_NeoPixel.h>
 #include <Wire.h>
+#include <SPI.h>
 #include <Adafruit_Sensor.h>
-#include <Adafruit_LSM303.h>
+#include <Adafruit_LIS3DH.h>
 
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
 
 #define PIN 13
-
 #define NUMPIXELS 3
 
-// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
-// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
-// example for more information on possible values.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-int delayval = 500; // delay for half a second
-
-Adafruit_LSM303 lsm;
+Adafruit_LIS3DH lis = Adafruit_LIS3DH();
 
 void setup() {
-  //pixels.begin(); // This initializes the NeoPixel library.
+  Serial.begin(9600);
+  
+  lis.setRange(LIS3DH_RANGE_4_G);   // 2, 4, 8 or 16 G!
+
+  lis.begin(0x18);
+  pixels.begin();
 }
 
 void loop() {
+  lis.read();
+  sensors_event_t event; 
+  lis.getEvent(&event);
 
-  /*for(int i=0;i<NUMPIXELS;i++){
+  //finds magnitude of acceleration
+  double accMag = sqrt(event.acceleration.x*event.acceleration.x + 
+            event.acceleration.y*event.acceleration.y + 
+            event.acceleration.z*event.acceleration.z);
 
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    pixels.setPixelColor(i, pixels.Color(255,255,255)); // Moderately bright green color.
-
+  for(int i=0;i<NUMPIXELS;i++){
+    if(accMag>10 || accMag<9.3)
+       pixels.setPixelColor(i, pixels.Color(0,0,255));
+    else
+      pixels.setPixelColor(i,pixels.Color(255,0,255));
+      
     pixels.show(); // This sends the updated pixel color to the hardware.
+  }
 
-    delay(delayval); // Delay for a period of time (in milliseconds).
+  Serial.print("Mag:  ");Serial.print(accMag);
+  Serial.println(" m/s^2 ");
 
-  }*/
+  Serial.println();
 
-  lsm.read();
-  Serial.print("Accel X: "); Serial.print((int)lsm.accelData.x); Serial.print(" ");
-  Serial.print("Y: "); Serial.print((int)lsm.accelData.y);       Serial.print(" ");
-  Serial.print("Z: "); Serial.println((int)lsm.accelData.z);     Serial.print(" ");
-  Serial.print("Mag X: "); Serial.print((int)lsm.magData.x);     Serial.print(" ");
-  Serial.print("Y: "); Serial.print((int)lsm.magData.y);         Serial.print(" ");
-  Serial.print("Z: "); Serial.println((int)lsm.magData.z);       Serial.print(" ");
   delay(1000);
-
-  /*pixels.setPixelColor(0, pixels.Color(0,0,255));
-  pixels.setPixelColor(1, pixels.Color(0,255,0));
-  pixels.setPixelColor(2, pixels.Color(255,0,0));
-  pixels.show();*/
 }
