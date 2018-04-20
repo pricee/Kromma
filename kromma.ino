0b364efa0ffadba10c37bf6fb2026aa51633fef7
Emma Price <emmalinprice@gmail.com>
2018-04-20 14:18:25 -0700

    Added latest version
    Added additional colors and changed color changing to a gradient instead of preset values

--- kromma.ino
+++ kromma.ino
@@ -11,8 +11,8 @@
   #include <avr/power.h>
 #endif
 
-#define PIN 13
-#define NUMPIXELS 3
+#define PIN 7
+#define NUMPIXELS 6
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
@@ -36,20 +36,43 @@ void loop() {
   double accMag = sqrt(event.acceleration.x*event.acceleration.x + 
             event.acceleration.y*event.acceleration.y + 
             event.acceleration.z*event.acceleration.z);
+  double accMagNoG = abs(accMag-9.8);
 
   for(int i=0;i<NUMPIXELS;i++){
-    if(accMag>10 || accMag<9.3)
+
+    //preset colors for different accelerations
+    /*if(accMag>12 || accMag<6.5)
        pixels.setPixelColor(i, pixels.Color(0,0,255));
+    else if(accMag>10 || accMag<9.1)
+      pixels.setPixelColor(i, pixels.Color(255,0,255));
     else
-      pixels.setPixelColor(i,pixels.Color(255,0,255));
+      pixels.setPixelColor(i,pixels.Color(255,0,50));*/
+      
+    //gradient
+    if(accMagNoG > 1.5){  
+      //pixels.setPixelColor(i,pixels.Color(255-accMagNoG*10,0,20+accMagNoG*20)); //pk --> pur
+      //pixels.setPixelColor(i,pixels.Color(20+accMagNoG*20,0,20+255-accMagNoG*10)); //b --> pk
+      //pixels.setPixelColor(i,pixels.Color(0,20+accMagNoG*20,20+255-accMagNoG*10)); //b --> g
+      pixels.setPixelColor(i,pixels.Color(0,20+255-accMagNoG*10,20+accMagNoG*20));//g-->b
+      //pixels.setPixelColor(i,pixels.Color(20+255-accMagNoG*10,20+accMagNoG*20,0)); //o --> g
+      //pixels.setPixelColor(i,pixels.Color(accMagNoG*20,255-accMagNoG*20,accMagNoG*20)); //g --> pur
+      }
+    else{
+      //pixels.setPixelColor(i,pixels.Color(255,0,50));//pk --> pur
+      //pixels.setPixelColor(i,pixels.Color(50,0,255));//b-->pk
+      //pixels.setPixelColor(i,pixels.Color(0,50,255));//b-->g
+      pixels.setPixelColor(i,pixels.Color(0,255,50));//g-->b
+      //pixels.setPixelColor(i,pixels.Color(255,50,0)); //o --> g
+      //pixels.setPixelColor(i,pixels.Color(0,255,0)); //g --> pur
+    }
       
     pixels.show(); // This sends the updated pixel color to the hardware.
   }
 
-  Serial.print("Mag:  ");Serial.print(accMag);
+  Serial.print("Mag:  ");Serial.print(accMagNoG);
   Serial.println(" m/s^2 ");
 
   Serial.println();
 
-  delay(1000);
+  delay(100);
 }
